b8d28197a5037e5507a9b1be148fdc72f096fb0e
Tigran <tantonian@thenuumfactory.fr>
2023-09-25 16:31:56 +0200

    test

---
+++ Azure-test.js
@@ -0,0 +1,8 @@
+// Ceci est un exemple de code Node.js LTS 18
+
+function afficherMessage() {
+    console.log("Bonjour, ceci est un exemple de code Node.js LTS 18 !");
+}
+
+// Appeler la fonction pour afficher le message
+afficherMessage();
